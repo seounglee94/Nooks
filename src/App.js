c8faa6538af33b8a1dd04f1ed5e8920a9cae1b7c
seounglee94 <dlrpwkd@naver.com>
2022-07-22 19:46:29 +0900

    useFadeIn

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
-const useBeforeLeave (onBefore) => {
-  if(typeof onBefore !=="function"){
+const useFadeIn = (duration = 1, delay = 0) => {
+  if (typeof duration !== "number" || typeof delay !== "number") {
     return;
   }
-  
-  const handle = (event) => {
-    const {clientY} = event;
-    if(clientY <= 0){
-      onBefore();}
-  }
-  useEffect(()=> {
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
-  }, []);
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opactiy ${duration}s ease-in-out ${delay}`;
+      current.style.opacity = 1;
+    }
+  });
+  return { ref: element, style: { opacity: 0 } };
 };
 
-  const App = () =>{
-    const begForLife = () => console.log("Pls dont leave")
-    return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
-    );
+export default function App() {
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 10);
+  return (
+    <div className="App">
+      <h1 {...fadeInH1}>Hello</h1>
+      <p>lorem ipsum lalalala</p>
+    </div>
+  );
 }
